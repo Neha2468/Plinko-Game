8acb0754381fca7431198feadeb761300a08ee9e
Neha2468 <81560725+Neha2468@users.noreply.github.com>
2021-07-28 21:10:32 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,68 @@
+const Engine = Matter.Engine;
+ const World= Matter.World;
+  const Bodies = Matter.Bodies;
+ const Constraint = Matter.Constraint;
+
+var engine, world;
+
+
+
+var ground;
+var plinkos = [];
+var particles = [];
+var divisions = [];
+var divisionHeight = 300;
+
+
+
+function setup() {
+  createCanvas(450,600);
+    engine = Engine.create();
+    world = engine.world;
+
+    ground = new Ground(width/2,589,width,10);
+
+    for(var k = 0; k<=innerWidth; k = k +80){
+      divisions.push(new Divisions(k, height-divisionHeight/2,10,divisionHeight));
+
+
+      for(var j = 40; j<=width; j=j+50){
+        plinkos.push(new Plinko(j,75));
+
+      }
+
+      for(var j = 15; j<=width-10; j=j+50){
+        plinkos.push(new Plinko(j,175));
+      }
+    }
+
+}
+
+
+
+function draw() {
+  background(0, 0, 0);  
+  drawSprites();
+  Engine.update(engine);
+  
+  ground.display();
+   
+  
+  for (var i = 0; i < plinkos.length; i++) {
+     
+     plinkos[i].display();
+     
+   }
+
+  for(j = 0; j < particles.length; j++){
+    particles[j].display();
+  }
+  for(var k = 0; k < divisions.length; k++){
+    divisions[k].display();
+  }
+
+  if(frameCount%40===0){
+    particles.push(new Particle(random(width/2-10, width/2+10), 10,10));
+  }
+
+}
